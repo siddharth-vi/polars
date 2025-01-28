2e8b855a48af84bbf197f55e5181ba55688c66ea
siddharth-vi <153494533+siddharth-vi@users.noreply.github.com>
2025-01-28 14:11:36 +0000

    Deploying to gh-pages from @ siddharth-vi/polars@96a2d0120a0ea298356c2302f4986a2219c1eaa7 🚀

---
+++ api/rust/dev/polars_io/predicates/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SpecializedColumnPredicateExpr"],"fn":["apply_predicate"],"struct":["BatchStats","ColumnPredicateExpr","ColumnStats"],"trait":["PhysicalIoExpr","StatsEvaluator"]};
